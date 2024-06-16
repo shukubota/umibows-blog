12b696e06b03adee8047c4f3237b58a5e1ae2cb8
shukubota <kubotashu0421@gmail.com>
2024-06-16 23:01:48 +0900

    着手禁止手の判定修正

--- app/igo/components/Stone.tsx
+++ app/igo/components/Stone.tsx
@@ -6,15 +6,15 @@ interface StoneProps {
 }
 
 const Stone: React.FC<StoneProps> = ({ color, position }) => {
-  const [row, col] = position;
+  const [col, row] = position;
   const backgroundColor = color === 'black' ? 'black' : 'white';
   return (
     <div
       className={`absolute rounded-full border border-black`}
       style={{
         backgroundColor, // 背景色を直接設定
-        top: `${(row + 1) * 10}%`,
-        left: `${(col + 1) * 10}%`,
+        top: `${row * 10}%`,
+        left: `${col * 10}%`,
         width: '8%',
         height: '8%',
         transform: 'translate(-50%, -50%)',
