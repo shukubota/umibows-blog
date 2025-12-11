fdef9d1cee83b1a44eb856d0004b5060a8d8a2ce
shukubota <kubotashu0421@gmail.com>
2025-12-11 23:33:52 +0900

    feat: Add a double pendulum simulation page and link it from the homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   const blogs = [
-    { id: 1, title: "First Blog Post" },
-    { id: 2, title: "Second Blog Post" },
-    { id: 3, title: "Third Blog Post" },
+    { id: "double-pendulum", title: "Double Pendulum Simulation", path: "/double-pendulum" },
+    { id: 2, title: "Second Blog Post", path: "/blog/2" },
+    { id: 3, title: "Third Blog Post", path: "/blog/3" },
   ];
 
   return (
@@ -14,7 +14,7 @@ export default function Home() {
         <ul className="space-y-4">
           {blogs.map((blog) => (
             <li key={blog.id} className="p-4 border rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-              <Link href={`/blog/${blog.id}`} className="text-xl font-semibold text-black dark:text-white">
+              <Link href={blog.path || `/blog/${blog.id}`} className="text-xl font-semibold text-black dark:text-white">
                 {blog.title}
               </Link>
             </li>
